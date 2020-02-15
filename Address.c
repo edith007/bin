063f3a8e48de302256863b95f3b6a9915d2a00a4
Siddharth Asthana <53316982+edith007@users.noreply.github.com>
2020-02-15 15:30:29 +0530

    Add files via upload

---
+++ Address.c
@@ -0,0 +1,18 @@
+#include<cs50.h>
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+    FILE *file = fopen("Phonebook.csv", "a");
+
+    //Get string from user
+    char *name = get_string("Name: ");
+    char *number = get_string("Number: ");
+
+    //Print(write) strings to file
+    fprintf(file, "%s,%s\n",name,number);
+
+    //close file
+    fclose(file);
+}
